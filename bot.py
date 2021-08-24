a36498604686e015cf9c5d0a82974ee6c8de6e1a
LINEKING2100 <63192745+LINEKING2100@users.noreply.github.com>
2021-08-24 11:11:05 +0900

    .

--- bot.py
+++ bot.py
@@ -13,6 +13,11 @@ async def on_ready():
     print("========================================================")
 
 @client.event
+async def on_message(message):
+    if message.content == ('!test'):
+        await message.channel.send("test")
+
+@client.event
 async def on_member_join(member):
     await member.guild.get_channel(687834516345454663).send(member.mention + """ HEAVYgiant( 헵자 )님 팬서버에 오신 걸 환영합니다! 메시지 하단에 있는 채널을 읽어주시고 활동을 시작해주세요! ( 공지, 밈 채널 제외 )
 규칙 : #💌│to-follow 
